068cd4b7e51d8884fd8c2be036995341805e6423
Valentin Laube <vlaube@cs-consulting.de>
2023-03-13 17:37:57 +0100

    Initial commit

---
+++ jv2-backend/src/main/java/com/jv2/jv2backend/Jv2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.jv2.jv2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jv2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jv2BackendApplication.class, args);
+	}
+
+}
---
+++ jv2-backend/src/test/java/com/jv2/jv2backend/Jv2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jv2.jv2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jv2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
